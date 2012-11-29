574b6d7b706ce347d317d5bd791f7a2647d84084
Andy Hurd <ahurd@adc4gis.com>
2012-11-28 20:27:51 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f436bb048302e457bee9a278feed04ca2a484ea5bd1556641d8f1f8971c349b3bb611284b76751c065ed946df8bd012ff1b180eeb9de90874454fccf79ce6b99'
